b2609355a5c5d82e12c9268af9b02d273e5cc799
Darshana <darshana3g@gmail.com>
2024-03-08 20:34:58 +0530

    added tween and other logic

--- player.gd
+++ player.gd
@@ -3,6 +3,8 @@ extends RigidBody3D
 @export_range(750, 3000) var thurst: float = 1000
 
 @export_range(-500, 500) var torque_thurst: float = 300
+
+var is_transitioning: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -21,7 +23,22 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		print("win")
-	if "Floor" in body.get_groups():
-		print("loss")
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			on_complete(body.file_path)
+		if "Floor" in body.get_groups():
+			on_crash()
+
+func on_complete(path):
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(path))
+	
+func on_crash():
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
